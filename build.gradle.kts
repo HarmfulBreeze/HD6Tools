f65415dd1a147f86abdc8ca17bf770402aeb91b7
Pierre GRASSER <piorrro33@outlook.com>
2021-03-18 21:27:02 +0100

    Added Gradle to the project and added picocli

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    application
+}
+
+group = "com.piorrro33"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.github.piorrro33.hd6tools.Main")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("info.picocli:picocli:4.6.1")
+    annotationProcessor("info.picocli:picocli-codegen:4.6.1")
+}
